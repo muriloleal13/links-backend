50518ba1c647f279037b7e3711a7c468f7cb32d0
Murilo Leal <mukaleal@gmail.com>
2020-06-29 22:32:55 -0300

    Storing accounts into database

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const { Account } = require('../models');
 
 const router = express.Router();
 
-router.get('/sign-up', (req, res) => {
-  return res.json('Sign up!');
+const saltRounds = 10;
+
+router.get('/sign-up', async (req, res) => {
+  const email = 'mukaleal@gmail.com';
+  const password = '123456';
+  const result = await Account.create({
+    email: email, 
+    password: bcrypt.hashSync(password, saltRounds)
+  });
+  return res.json(result);
 });
 
 router.get('/sign-in', (req, res) => {
