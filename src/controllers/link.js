5d0b32e22756d90df3af1d20f30c3a67b6ac2546
Murilo Leal <mukaleal@gmail.com>
2020-07-01 17:18:30 -0300

    Verify JWT

--- src/controllers/link.js
+++ src/controllers/link.js
@@ -5,7 +5,7 @@ const { Link } = require('../models');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  const accountId = 1;//req.id;
+  const { accountId } = req;
 
   const link = await Link.findAll({ where: { accountId } });
   if(!link) return res.jsonNotFound();
@@ -14,7 +14,7 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
-  const accountId = 1;//req.id;
+  const { accountId } = req;
   const { id } = req.params;
 
   const link = await Link.findOne({ where: { id, accountId } });
@@ -24,8 +24,8 @@ router.get('/:id', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  const accountId = 1;//req.id;
-  const { label, url, isSocial } = req.body;
+  const { accountId, body } = req;
+  const { label, url, isSocial } = body;
 
   const image = 'https://google.com/image.jpg';
   
@@ -35,9 +35,8 @@ router.post('/', async (req, res) => {
 });
 
 router.put('/:id', async (req, res) => {
-  const accountId = 1;//req.id;
+  const { accountId, body } = req;
   const { id } = req.params;
-  const { body } = req;
   const fields = ['label', 'url', 'isSocial'];
 
   const link = await Link.findOne({ where: { id, accountId } });
@@ -54,7 +53,7 @@ router.put('/:id', async (req, res) => {
 });
 
 router.delete('/:id', async (req, res) => {
-  const accountId = 1;//req.id;
+  const { accountId } = req;
   const { id } = req.params;
 
   const link = await Link.findOne({ where: { id, accountId } });
