3770de96054041236fdbd4df026f476d792da7d1
Murilo Leal <mukaleal@gmail.com>
2020-07-01 16:24:30 -0300

    Jwt implemented

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,28 +1,43 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const { Account } = require('../models');
-const { accountSignUp } = require('../validators/account')
+const { accountSignUp, accountSignIn } = require('../validators/account')
 const { getMessage } = require('../helpers/messages');
+const { generateJwt, generateRefreshJwt } = require('../helpers/jwt');
 
 const router = express.Router();
 
 const saltRounds = 10;
 
-router.get('/sign-up', accountSignUp, async (req, res) => {
+router.post('/sign-up', accountSignUp, async (req, res) => {
   const { email, password } = req.body;
 
-  const account = await Account.findOne({where: {email}});
+  const account = await Account.findOne({ where: { email } });
   if(account) return res.jsonBadRequest(null, getMessage('account.signup.email_exists'));
 
   const newAccount = await Account.create({
     email: email, 
     password: bcrypt.hashSync(password, saltRounds)
   });
-  return res.jsonOK(newAccount, getMessage('account.signup.success'));
+
+  const token = generateJwt({ id: newAccount.id });
+  const refreshToken  = generateRefreshJwt({ id: newAccount.id });
+
+  return res.jsonOK(newAccount, getMessage('account.signup.success'), { token, refreshToken });
 });
 
-router.get('/sign-in', (req, res) => {
-  return res.jsonOK(null);
+router.post('/sign-in', accountSignIn, async (req, res) => {
+  const { email, password } = req.body;
+
+  const account = await Account.findOne({ where: { email } });
+  
+  const match = account ? bcrypt.compareSync(password, account.password) : null;
+  if(!match) return res.jsonBadRequest(null, getMessage('account.signin.invalid'));
+  
+  const token = generateJwt({ id: account.id });
+  const refreshToken  = generateRefreshJwt({ id: account.id });
+
+  return res.jsonOK(account, getMessage('account.signin.success'), { token, refreshToken });
 });
 
 module.exports = router;
--- src/validators/account.js
+++ src/validators/account.js
@@ -1,16 +1,23 @@
 const Joi = require('@hapi/joi');
 const { getValidatorError } = require('../helpers/validator');
 
+const rules = {
+  email: Joi.string().email().required(),
+  password: Joi.string().pattern(new RegExp('^[a-zA-Z-0-9]{3,30}$')),
+  password_confirmation: Joi.string().valid(Joi.ref('password')).required()
+};
+const options = {abortEarly: false };
+
+
 const accountSignUp = (req, res, next) => {
   const { email, password, password_confirmation } = req.body;
 
   const schema = Joi.object({
-    email: Joi.string().email().required(),
-    password: Joi.string().pattern(new RegExp('^[a-zA-Z-0-9]{3,30}$')),
-    password_confirmation: Joi.string().valid(Joi.ref('password')).required(),
+    email: rules.email,
+    password: rules.password,
+    password_confirmation: rules.password_confirmation,
   });
 
-  const options = {abortEarly: false };
   const { error } = schema.validate({ email, password, password_confirmation }, options);
   if(error) {
     const messages = getValidatorError(error, 'account.signup');
@@ -20,4 +27,21 @@ const accountSignUp = (req, res, next) => {
   next();
 };
 
-module.exports = { accountSignUp };
+const accountSignIn = (req, res, next) => {
+  const { email, password } = req.body;
+
+  const schema = Joi.object({
+    email: rules.email,
+    password: rules.password,
+  });
+
+  const { error } = schema.validate({ email, password }, options);
+  if(error) {
+    const messages = getValidatorError(error, 'account.signin');
+    return res.jsonBadRequest(null, null, { messages });
+  }
+
+  next();
+};
+
+module.exports = { accountSignUp, accountSignIn };
